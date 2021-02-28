59e9afcdb9d4e0155744a09240e6cd65bd70d600
Miroslav Šafář <miroslav.safar@znojmo.cz>
2021-02-28 22:56:34 +0100

    Rozhraní matematické knihovny; Fixes #2

---
+++ src/math_lib.py
@@ -0,0 +1,30 @@
+def sum(a, b):
+    pass
+
+
+def sub(a, b):
+    pass
+
+
+def multiply(a, b):
+    pass
+
+
+def divide(a, b):
+    pass
+
+
+def factorial(n):
+    pass
+
+
+def power(a, exp):
+    pass
+
+
+def nth_root(x, n):
+    pass
+
+
+def sin(x):
+    pass
