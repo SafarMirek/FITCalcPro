0fe00cfe729b474035c7ce1df0d0e4d1840aa1df
Miroslav Šafář <miroslav.safar@znojmo.cz>
2021-03-27 22:50:10 +0100

    Implementace funkce eval

--- src/math_interpreter.py
+++ src/math_interpreter.py
@@ -1,5 +1,37 @@
 import math_lib
 
 
+##
+# @brief Vyčíslí výraz dle priority operací a vrátí jeho hodnotu
+# @param members List členů výrazu
+# @param operations List operací mezi členy
+# @return Výsledná hodnota výrazu
+#
 def eval(members, operations):
-    pass
+    i = 0
+    while i < len(operations):
+        if operations[i] == "*":
+            members[i] = math_lib.multiply(members[i], members[i + 1])
+            del members[i + 1]
+            del operations[i]
+        elif operations[i] == "/":
+            members[i] = math_lib.divide(members[i], members[i + 1])
+            del members[i + 1]
+            del operations[i]
+        else:
+            i = i + 1
+
+    i = 0
+    while i < len(operations):
+        if operations[i] == "+":
+            members[i] = math_lib.sum(members[i], members[i + 1])
+            del members[i + 1]
+            del operations[i]
+        elif operations[i] == "-":
+            members[i] = math_lib.sub(members[i], members[i + 1])
+            del members[i + 1]
+            del operations[i]
+        else:
+            i = i + 1
+
+    return members[0]
