504e6d5b12b7b31218fabfb8ebaaf6995de7828c
Miroslav Šafář <miroslav.safar@znojmo.cz>
2021-04-23 15:00:22 +0200

    Přidáno nastavení licence na GPL3 do release.sh

--- releases/release.sh
+++ releases/release.sh
@@ -9,7 +9,7 @@ VERSION="$1"
 
 eval "rm -fR fitcalcpro-$VERSION fitcalcpro-$VERSION.tar.gx && cp -r package-files fitcalcpro-$VERSION" &&
 eval "tar -czvf fitcalcpro-$VERSION.tar.gx fitcalcpro-$VERSION" &&
-eval "cd fitcalcpro-$VERSION && dh_make -e xsafar23@stud.fit.vutbr.cz -n -s -f ../fitcalcpro-$VERSION.tar.gx" &&
+eval "cd fitcalcpro-$VERSION && dh_make -e xsafar23@stud.fit.vutbr.cz -c gpl3 -n -s -f ../fitcalcpro-$VERSION.tar.gx" &&
 eval "cd .. && cp debian/install fitcalcpro-$VERSION/debian/install" &&
 eval "cp -i -f debian/control fitcalcpro-$VERSION/debian/control" && 
 eval "cd fitcalcpro-$VERSION && dpkg-buildpackage -rfakeroot -uc -b"
