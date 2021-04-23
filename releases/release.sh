c290c806a050136b3b0085483be9c10d05566cf5
Miroslav Šafář <miroslav.safar@znojmo.cz>
2021-04-23 17:44:38 +0200

    Odstraněno potvrzení u přepisu debian/control

--- releases/release.sh
+++ releases/release.sh
@@ -7,10 +7,12 @@ fi
 
 VERSION="$1"
 
-eval "rm -fR fitcalcpro-$VERSION fitcalcpro-$VERSION.tar.gx && cp -r package-files fitcalcpro-$VERSION" &&
+eval "rm -fR fitcalcpro-$VERSION fitcalcpro-$VERSION.tar.gx" &&
+eval "cp -r package-files fitcalcpro-$VERSION" &&
 eval "tar -czvf fitcalcpro-$VERSION.tar.gx fitcalcpro-$VERSION" &&
-eval "cd fitcalcpro-$VERSION && dh_make -e xsafar23@stud.fit.vutbr.cz -c gpl3 -n -s -f ../fitcalcpro-$VERSION.tar.gx" &&
+eval "cd fitcalcpro-$VERSION" &&
+eval "dh_make -e xsafar23@stud.fit.vutbr.cz -c gpl3 -n -s -f ../fitcalcpro-$VERSION.tar.gx" &&
 eval "cd .. && cp debian/install fitcalcpro-$VERSION/debian/install" &&
-eval "cp -i -f debian/control fitcalcpro-$VERSION/debian/control" && 
+eval "yes | cp -i -f debian/control fitcalcpro-$VERSION/debian/control" && 
 eval "cd fitcalcpro-$VERSION && dpkg-buildpackage -rfakeroot -uc -b"
 
